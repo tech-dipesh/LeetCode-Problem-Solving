bd99aadba7ffaa389632800d87acae96c46f0d1a
Dipesh Tech <dipeshsharmagautam@outlook.com>
2025-10-22 23:02:00 +0530

    Time: 3 ms (29.94%) | Memory: 28.2 MB (28.80%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //Hash Map o(n)+o(n)=o(n) and s.c.: o(n)
-        int half=nums.size()/2;
-    unordered_map<int, int>mp;
-    for (int i : nums) 
-        mp[i]++;
-        for(int i=0;i<nums.size();i++){
-    //  for (auto i: mp) {
-        // if (i.second>half){
-            //  return i.first;
-            if(mp[nums[i]]>half){
-                return nums[i];
-            }
-    }
-    return 0;
+        // First sort the element o(nlogn) and just return the half index array element
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
     }
 };
