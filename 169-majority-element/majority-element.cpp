f3e25a41c584119e3015698de8911c8f8c79d166
Dipesh Tech <dipeshsharmagautam@outlook.com>
2025-10-22 22:57:25 +0530

    Time: 1 ms (37.36%) | Memory: 28.2 MB (63.27%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -6,11 +6,14 @@ public:
     unordered_map<int, int>mp;
     for (int i : nums) 
         mp[i]++;
-     for (auto i: mp) {
-        if (i.second>half){
-             return i.first;
-        }
+        for(int i=0;i<nums.size();i++){
+    //  for (auto i: mp) {
+        // if (i.second>half){
+            //  return i.first;
+            if(mp[nums[i]]>half){
+                return nums[i];
+            }
     }
-    return -1;
+    return 0;
     }
 };
