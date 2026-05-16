d16886b22cdac88907c23947960ad7252a14dace
Dipendra Sharma <dipeshsharmagautam@outlook.com>
2026-05-16 22:45:02 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (90.00%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[0];
+     sort(nums.begin(), nums.end());
+     return nums[0];   
     }
 };
